67329f8b3677c4c5ca85caa9344b7e53032c7600
bin.yang <bin.yang@telink-semi.com>
2024-12-06 20:23:37 +0800

    feat(demo): update sampleLight demo. NOTE: Due to changes in the length of data stored in NV, such as scenarios, it is not compatible with previous sampleLight demo.

--- tl_zigbee_sdk/apps/sampleLight/sampleLight.c
+++ tl_zigbee_sdk/apps/sampleLight/sampleLight.c
@@ -225,7 +225,7 @@ void app_task(void)
 
 		report_handler();
 
-#if 0/* NOTE: If set to '1', the latest status of lighting will be stored. */
+#if 1/* NOTE: If set to '1', the latest status of lighting will be stored. */
 		sampleLightAttrsChk();
 #endif
 	}
@@ -254,9 +254,13 @@ void user_init(bool isRetention)
 {
 	(void)isRetention;
 
+	/* Register except handler for test */
+	sys_exceptHandlerRegister(sampleLightSysException);
+
 	/* Initialize LEDs*/
 	led_init();
-	hwLight_init();
+	
+	light_init();
 
 	//factoryRst_init();
 
@@ -265,9 +269,6 @@ void user_init(bool isRetention)
 
 	/* Initialize user application */
 	user_app_init();
-
-	/* Register except handler for test */
-	sys_exceptHandlerRegister(sampleLightSysException);
 
 	/* Adjust light state to default attributes*/
 	light_adjust();
@@ -286,9 +287,36 @@ void user_init(bool isRetention)
 	}
 
     /* Set default reporting configuration */
-    u8 reportableChange = 0x00;
-    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID, ZCL_CLUSTER_GEN_ON_OFF, ZCL_ATTRID_ONOFF,
-    						0x0000, 0x003c, (u8 *)&reportableChange);
+	u8 reportableChange[2] = {0x00, 0x00};
+    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID,
+    						ZCL_CLUSTER_GEN_ON_OFF, ZCL_ATTRID_ONOFF,
+    						1, 120, (u8 *)&reportableChange);//min = 1s, max = 120s
+#if ZCL_LEVEL_CTRL_SUPPORT
+    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID,
+    						ZCL_CLUSTER_GEN_LEVEL_CONTROL, ZCL_ATTRID_LEVEL_CURRENT_LEVEL,
+    						1, 120, (u8 *)&reportableChange);
+#endif
+#if COLOR_RGB_SUPPORT
+    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID,
+    						ZCL_CLUSTER_LIGHTING_COLOR_CONTROL, ZCL_ATTRID_CURRENT_HUE,
+							1, 120, (u8 *)&reportableChange);
+    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID,
+    						ZCL_CLUSTER_LIGHTING_COLOR_CONTROL, ZCL_ATTRID_CURRENT_SATURATION,
+							1, 120, (u8 *)&reportableChange);
+#ifndef COLOR_X_Y_DISABLE
+    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID,
+    						ZCL_CLUSTER_LIGHTING_COLOR_CONTROL, ZCL_ATTRID_CURRENT_X,
+							1, 120, (u8 *)&reportableChange);
+    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID,
+    						ZCL_CLUSTER_LIGHTING_COLOR_CONTROL, ZCL_ATTRID_CURRENT_Y,
+							1, 120, (u8 *)&reportableChange);
+#endif
+#endif
+#if COLOR_CCT_SUPPORT
+    bdb_defaultReportingCfg(SAMPLE_LIGHT_ENDPOINT, HA_PROFILE_ID,
+    						ZCL_CLUSTER_LIGHTING_COLOR_CONTROL, ZCL_ATTRID_COLOR_TEMPERATURE_MIREDS,
+							1, 120, (u8 *)&reportableChange);
+#endif
 
     /* Initialize BDB */
 	bdb_init((af_simple_descriptor_t *)&sampleLight_simpleDesc, &g_bdbCommissionSetting, &g_zbDemoBdbCb, 1);
